dadf1e28fc47059063eaf66e9e7f1943aa22fa4b
りぶ <g.s.sequence@gmail.com>
2019-09-13 15:58:42 +0900

    Add .config/ and neovim configuration

--- tools/install.sh
+++ tools/install.sh
@@ -7,6 +7,16 @@ for p in $DOTPATH/.??*; do
   [[ "$f" == ".git" ]] && continue
   [[ "$f" == ".gitignore" ]] && continue
   [[ "$f" == ".DS_Store" ]] && continue
+  if [[ "$f" == ".config" ]] ; then
+    if [[ ! -e "$HOME/.config" ]]; then
+      mkdir "$HOME/.config"
+    fi
+    for pp in $p/*; do
+      ff=$(basename $pp)
+      ln -snfv "$pp" "$HOME/.config/$ff"
+    done
+    continue
+  fi
 
   ln -snfv "$p" "$HOME/$f"
 done
